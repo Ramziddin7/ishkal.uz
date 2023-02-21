b717186033fbdcb48eeed7d3ebb1b2e0e1e265d7
Ramziddin7 <rustamovramziddin7@gmail.com>
2023-02-21 16:58:04 +0500

    a

--- app/Http/Resources/FieldRelationResource.php
+++ app/Http/Resources/FieldRelationResource.php
@@ -23,7 +23,7 @@ class FieldRelationResource extends JsonResource
             'description'=>$this->description,
             'created_at'=>$this->created_at->diffForHumans(),
             'updated_at'=>$this->updated_at->diffForHumans(),
-            'university'=>UniversityResource::collection($this->university),
+            'university'=> new UniversityResource($this->university),
         ];
     }
 }
--- app/Http/Resources/UniversityRelationResource.php
+++ app/Http/Resources/UniversityRelationResource.php
@@ -25,7 +25,7 @@ class UniversityRelationResource extends JsonResource
             'image'=>asset($this->image),
             'created_at'=>$this->created_at->diffForHumans(),
             'updated_at'=>$this->updated_at->diffForHumans(),
-            'country'=>CountryResource::collection($this->country),
+            'country'=>new CountryResource($this->country),
             'fields'=>FieldResource::collection($this->field)
         ];
     }
